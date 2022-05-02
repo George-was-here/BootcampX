2633ed03a1eb4acd341a284d7e70c4d2a5b94a1d
George-was-here <george.burt@gmail.com>
2022-05-02 19:52:42 +0000

    Querries 1 implemented

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -1,4 +1,47 @@
+--Students without Github--
+
 SELECT id, name, email, cohort_id
 FROM students
 WHERE github IS NULL
+ORDER BY cohort_id;
+
+--Students in a Given Cohort--
+
+SELECT id, name 
+FROM students 
+WHERE cohort_id = 1
+ORDER BY name;
+
+--Total Students in Cohort--
+
+SELECT count(*)
+FROM students
+WHERE cohort_id = 1 OR cohort_id = 2 OR cohort_id = 3;
+
+--No Contact Details--
+
+SELECT name, id, cohort_id
+FROM students
+WHERE email IS NULL
+OR phone IS NULL;
+
+--Non Gmail Students--
+
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail.com'
+AND phone IS NULL;
+
+--Currently Enrolled--
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
+
+--Graduates without Github--
+
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL AND github IS NULL
 ORDER BY cohort_id;
