0a5e34d8060acb852ad404bf32f0779101431fa6
George-was-here <george.burt@gmail.com>
2022-05-04 04:13:22 +0000

    Implementing total assitances

---
+++ 4_queries/most_confusing_assignments.sql
@@ -0,0 +1,8 @@
+SELECT assignments.id, name, day, chapter, COUNT(assistance_requests.id) as total_assistances
+FROM assignments
+JOIN assistance_requests ON assistance_requests.assignment_id = assignments.id
+GROUP BY assignments.id
+ORDER BY total_assistances desc;
+
+
+Select the assignment's id, day, chapter, name and the total assistances.
